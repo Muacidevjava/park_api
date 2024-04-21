d0d6ee7b4441bcb020430801e7110bd8151db00e
Muacidevjava <muaci@yahoo.com>
2024-04-21 14:51:33 -0300

    recursos para criar novo usuario

--- src/main/java/com/muacidev/demoparkapi/service/UsuarioService.java
+++ src/main/java/com/muacidev/demoparkapi/service/UsuarioService.java
@@ -1,11 +1,17 @@
 package com.muacidev.demoparkapi.service;
 
+import com.muacidev.demoparkapi.entity.Usuario;
 import com.muacidev.demoparkapi.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 @RequiredArgsConstructor
 @Service
 public class UsuarioService {
     private UsuarioRepository usuarioRepository;
-
+   @Transactional
+    public Usuario salvar(Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
 }

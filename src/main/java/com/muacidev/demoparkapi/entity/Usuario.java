dd0590a2c93522435ca361363948ad889ef71aca
Muacidevjava <muaci@yahoo.com>
2024-04-21 14:31:13 -0300

    criando a classe Usuario

--- src/main/java/com/muacidev/demoparkapi/entity/Usuario.java
+++ src/main/java/com/muacidev/demoparkapi/entity/Usuario.java
@@ -1,8 +1,19 @@
 package com.muacidev.demoparkapi.entity;
 
 import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Getter
+@Setter
+@NoArgsConstructor
+
 @Entity
 @Table(name = "usuarios")
 public class Usuario  implements Serializable {
@@ -10,13 +21,43 @@ public class Usuario  implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
+    @Column(name = "username", nullable = false,unique = true,length = 100)
     private String username;
+    @Column(name = "password",nullable = false,length = 200)
     private String password;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role",nullable = false,length = 25)
     private Role role;
+    @Column(name = "data_criacao")
+    private LocalDateTime daraCriacao;
+    @Column(name = "data_modificacao")
+    private LocalDateTime dataModificacao;
+    @Column(name = "criado_po")
+    private String criadoPor;
+    @Column(name = "modificado_por")
+    private String modificadoPor;
+
 
     public enum Role{
         ROLE_ADMIN, ROLE_CLIENTE
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Usuario usuario)) return false;
+        return Objects.equals(getId(), usuario.getId());
+    }
 
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getId());
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "id=" + id +
+                '}';
+    }
 }

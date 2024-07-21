2db6c700395088e86f7c7abf68d4c7308ab00cda
muaci <muaci@yahoo.com>
2024-07-21 18:54:12 -0300

    excluindo import não usado

--- src/main/java/com/muacidev/demoparkapi/entity/Usuario.java
+++ src/main/java/com/muacidev/demoparkapi/entity/Usuario.java
@@ -17,23 +17,33 @@ import java.util.Objects;
 @Entity
 @Table(name = "usuarios")
 public class Usuario  implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
+
     @Column(name = "username", nullable = false,unique = true,length = 100)
     private String username;
+
     @Column(name = "password",nullable = false,length = 200)
     private String password;
+
     @Enumerated(EnumType.STRING)
     @Column(name = "role",nullable = false,length = 25)
     private Role role;
+
     @Column(name = "data_criacao")
     private LocalDateTime daraCriacao;
+
     @Column(name = "data_modificacao")
     private LocalDateTime dataModificacao;
+
     @Column(name = "criado_po")
     private String criadoPor;
+
     @Column(name = "modificado_por")
     private String modificadoPor;
 

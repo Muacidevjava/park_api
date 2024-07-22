e0afb902d0ecb9cfdca0b4572983c67f9e8deb00
muaci <muaci@yahoo.com>
2024-07-22 11:17:14 -0300

    validando usuários

--- src/main/java/com/muacidev/demoparkapi/web/dto/UsuarioCreateDto.java
+++ src/main/java/com/muacidev/demoparkapi/web/dto/UsuarioCreateDto.java
@@ -1,5 +1,8 @@
 package com.muacidev.demoparkapi.web.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Getter
@@ -9,8 +12,12 @@ import lombok.*;
 @ToString
 public class UsuarioCreateDto {
 
+    @NotBlank
+    @Email( message = "Formato do email inválido",regexp = "^[\\w!#$%&'*+/=?`{|}~^-]+(?:\\.[\\w!#$%&'*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$")
     private String username;
 
+    @NotBlank
+    @Size(min = 6, max = 6 )
     private String password;
 
 }

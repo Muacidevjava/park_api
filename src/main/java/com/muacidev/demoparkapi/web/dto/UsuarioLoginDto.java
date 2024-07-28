61220327be98d23177d0ac71cabfc202475533a9
muaci <muaci@yahoo.com>
2024-07-28 05:40:26 -0300

    criando controller de Autenticação

---
+++ src/main/java/com/muacidev/demoparkapi/web/dto/UsuarioLoginDto.java
@@ -0,0 +1,24 @@
+package com.muacidev.demoparkapi.web.dto;
+
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class UsuarioLoginDto {
+
+    @NotBlank
+    @Email( message = "Formato do email inválido",regexp = "^[\\w!#$%&'*+/=?`{|}~^-]+(?:\\.[\\w!#$%&'*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$")
+    private String username;
+
+
+    @NotBlank
+    @Size(min = 6,max = 6)
+    private String password;
+}

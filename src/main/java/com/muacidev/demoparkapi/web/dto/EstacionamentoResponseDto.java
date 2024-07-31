b693a78edf298f4d8fec185bfe074f3ddab3ea83
muaci <muaci@yahoo.com>
2024-07-31 20:48:42 -0300

    A classe EstacionamentoController e o check-in de um veículo

--- src/main/java/com/muacidev/demoparkapi/web/dto/EstacionamentoResponseDto.java
+++ src/main/java/com/muacidev/demoparkapi/web/dto/EstacionamentoResponseDto.java
@@ -1,6 +1,8 @@
 package com.muacidev.demoparkapi.web.dto;
 
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,6 +16,7 @@ import java.time.LocalDateTime;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class EstacionamentoResponseDto {
 
     private String placa;
@@ -28,8 +31,10 @@ public class EstacionamentoResponseDto {
 
     private  String recibo;
 
+    @JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss")
     private LocalDateTime dataEntrada;
 
+    @JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss")
     private LocalDateTime dataSaida;
 
     private String vagaCodigo;

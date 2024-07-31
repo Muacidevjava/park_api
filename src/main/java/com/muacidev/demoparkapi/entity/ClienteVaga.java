afd041dc159e8f4443df0e39d25d58b6674d914c
muaci <muaci@yahoo.com>
2024-07-31 19:30:08 -0300

    Criando o mapeamento para a tabela Clientes_Tem_Vagas

--- src/main/java/com/muacidev/demoparkapi/entity/ClienteVaga.java
+++ src/main/java/com/muacidev/demoparkapi/entity/ClienteVaga.java
@@ -20,7 +20,7 @@ import java.util.Objects;
 @Entity
 @Table(name = "cliente_tem_vagas")
 @EntityListeners(AuditingEntityListener.class)
-public class ClienteVaga implements Serializable {
+public class ClienteVaga  {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
